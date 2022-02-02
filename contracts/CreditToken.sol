2ab247cb0a0a6cdfb7019df8d95007d69cc0c9b5
Gizmotronn <droidology@allianceofdroids.org.au>
2022-02-02 15:41:40 +1100

    🧲🔑 -> Adding mintMyNft function to smart contract for #3

--- contracts/CreditToken.sol
+++ contracts/CreditToken.sol
@@ -6,7 +6,16 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract CreditToken is ERC721, Ownable {
+    uint256 COUNTER = 1; // Incremental function for indexing of NFTs, first NFT starts at 1 index
+
     constructor(string memory _name, string memory _symbol)
-    ERC721(_name, _symbol)
+        ERC721(_name, _symbol)
     {}
+
+    // Minting function
+    function mintMyNft(address _to) public onlyOwner {
+        // Only the owner of the contract can call this function
+        _safeMint(_to, COUNTER); // Who is the NFT for?, what is the identifier of the NFT that's being minted?
+        COUNTER++;
+    }
 }
