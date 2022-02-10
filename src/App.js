170e622e1d4f7aa7f921f9b8254214f213018e81
Gizmotronn <droidology@allianceofdroids.org.au>
2022-02-10 16:35:07 +0800

    📅🎧 -> Configured blockchain.data in App.js to work for #3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React, {useEffect} from "react";
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {connect} from "./redux/blockchain/blockchainActions";
+import { fetchData } from './redux/data/dataActions';
 import * as s from "./styles/globalStyles";
 
 function App() {
   const dispatch = useDispatch();
   const blockchain = useSelector(state => state.blockchain);
+  const data = useSelector(state => state.data);
 
-  console.table(blockchain);
+  console.log(data);
+  //console.table(blockchain);
 
   useEffect(() => {
-  }, [dispatch]);
+    if(blockchain.account != "") {
+      dispatch(fetchData());
+    }
+  },[blockchain.account]); // Run this useEffect whenever the blockchain's account is updated
 
   return <s.Screen>
     {blockchain.account !== "" || blockchain.gearToken !== null ? (
-    <s.Container flex={1} ai={"center"} jc={"center"}>
-      <s.SpacerSmall />
-      <button onClick={(e) => {
-        e.preventDefault(); // prevent default form submit
-        dispatch(connect());
-      }}>CONNECT</button>
-    </s.Container>
-    ) : (
-      <s.Container>
+      <s.Container flex={1} ai={"center"} jc={"center"}>
         <s.TextTitle>
-          Connect to our Game
+          Connect to our game
         </s.TextTitle>
+        <s.SpacerSmall />
+        <button onClick={(e) => {
+          e.preventDefault(); // prevent default form submit
+          dispatch(connect());
+        }}>CONNECT</button>
+      </s.Container>
+    ) : (
+      <s.Container ai={"center"} style={{ padding: "24px" }}>
+        <s.TextTitle>
+          Welcome to our game
+        </s.TextTitle>
+        <s.SpacerSmall />
       </s.Container>
     )}
   </s.Screen>
--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -6,100 +6,79 @@ import GearToken from "../../build/contracts/GearToken.json";
 
 const connectRequest = () => {
     return {
-        type: "CONNECTION_REQUEST",
+      type: "CONNECTION_REQUEST",
     };
 };
-
+  
 const connectSuccess = (payload) => {
     return {
-        type: "CONNECTION_SUCCESS",
+      type: "CONNECTION_SUCCESS",
+      payload: payload,
     };
 };
-
+  
 const connectFailed = (payload) => {
     return {
-        type: "CONNECTION_FAILED",
-        payload: payload,
+      type: "CONNECTION_FAILED",
+      payload: payload,
     };
 };
-
+  
 const updateAccountRequest = (payload) => {
     return {
-        type: "UPDATE_ACCOUNT",
-        payload: payload,
+      type: "UPDATE_ACCOUNT",
+      payload: payload,
     };
 };
-
+  
 export const connect = () => {
     return async (dispatch) => {
-        dispatch(connectRequest());
-        if (window.ethereum) { // If the browser visiting the site is blockchain-compatible (i.e. it has Metamask installed)
-            let web3 = new Web3(window.ethereum); // Connect to the Ethereum network with a new web3 object
-            try {
-                const accounts = await window.ethereum.request({
-                    method: "eth_accounts",
-                });
-                console.log("Account, ", accounts[0])
-                const networkId = await window.ethereum.request({
-                    method: "net_version",
-                });
-                console.log("Network Id ", networkId);
-                if (networkId == 137) {
-                    const gearToken = new web3.eth.Contract(
-                        GearToken.abi,
-                        "0xbDE17f0284769996613652642dCab1db36Fcb54E"
-                    );
-                    dispatch(
-                        connectSuccess({
-                            account: accounts[0],
-                            gearToken: gearToken,
-                            web3: web3
-                        })
-                    );
-                    // Add listeners start
-                    window.ethereum.on("accountsChanged", (accounts) => {
-                        dispatch(updateAccount(accounts[0]));
-                    });
-                    window.ethereum.on("chainChanged", () => {
-                        window.location.reload();
-                    });
-                    // Listeners end
-                }  else {
-          dispatch(connectFailed("Change network to Polygon."));
+      dispatch(connectRequest());
+      if (window.ethereum) {
+        let web3 = new Web3(window.ethereum);
+        try {
+          const accounts = await window.ethereum.request({
+            method: "eth_accounts",
+          });
+          const networkId = await window.ethereum.request({
+            method: "net_version",
+          });
+          console.log(networkId);
+          if (networkId == 137) {
+            const gearToken = new web3.eth.Contract(
+                GearToken.abi,
+                "0xbDE17f0284769996613652642dCab1db36Fcb54E"
+            );
+            dispatch(
+                connectSuccess({
+                    account: accounts[0],
+                    gearToken: gearToken,
+                    web3: web3
+                })
+            );
+            // Add listeners start
+            window.ethereum.on("accountsChanged", (accounts) => {
+              dispatch(updateAccount(accounts[0]));
+            });
+            window.ethereum.on("chainChanged", () => {
+              window.location.reload();
+            });
+            // Add listeners end
+          } else {
+            dispatch(connectFailed("Change network to Polygon."));
+          }
+        } catch (err) {
+          dispatch(connectFailed("Something went wrong."));
         }
-      } catch (err) {
-        dispatch(connectFailed("Something went wrong."));
+      } else {
+        dispatch(connectFailed("Install Metamask."));
       }
-    } else {
-      dispatch(connectFailed("Install Metamask."));
-    }
-  };
+    };
 };
-
+  
 export const updateAccount = (account) => {
-  return async (dispatch) => {
-    dispatch(updateAccountRequest({ account: account }));
-    //dispatch(fetchData(account));
-  };
+    return async (dispatch) => {
+      dispatch(updateAccountRequest({ account: account }));
+      //dispatch(fetchData(account));
+    };
 };
-
-                
-                /*
-                else {
-                    dispatch(connectFailed("Change network to Polygon."));
-                }
-            } catch (err) {
-                dispatch(connectFailed("Error: " + err)); // If there's an error connecting
-            }
-        } else {
-            dispatch(connectFailed('Install Metamask.')); // Sends (action) type "CONNECTION_FAILED" with payload "Install Metamask" as Metamask is not installed on the browser accessing the site
-        }
-    };
-        
-    export const updateAccount = (account) => {
-        return async (dispatch) => {
-            dispatch(updateAccountRequest({ account: account }));
-            dispatch(fetchData(account));
-        };
-    };
-};*/
