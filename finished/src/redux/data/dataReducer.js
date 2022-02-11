468d4d39787786f8f8a50887e5dc6e722a75bec7
Gizmotronn <droidology@allianceofdroids.org.au>
2022-02-11 12:12:42 +0800

    🔑🛎 -> Finished project added (problem with account (import * as s. in app.js) fixed but Polygon + ganache causing issues) #3

---
+++ finished/src/redux/data/dataReducer.js
@@ -0,0 +1,35 @@
+const initialState = {
+  loading: false,
+  allGears: [],
+  allOwnerGears: [],
+  error: false,
+  errorMsg: "",
+};
+
+const dataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "CHECK_DATA_REQUEST":
+      return {
+        ...initialState,
+        loading: true,
+      };
+    case "CHECK_DATA_SUCCESS":
+      return {
+        ...initialState,
+        loading: false,
+        allGears: action.payload.allGears,
+        allOwnerGears: action.payload.allOwnerGears,
+      };
+    case "CHECK_DATA_FAILED":
+      return {
+        ...initialState,
+        loading: false,
+        error: true,
+        errorMsg: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default dataReducer;
--- src/App.js
+++ src/App.js
@@ -9,37 +9,107 @@ function App() {
   const dispatch = useDispatch();
   const blockchain = useSelector(state => state.blockchain);
   const data = useSelector(state => state.data);
+  //const [loading, setLoading] = useState(false);
 
   console.log(data);
   //console.table(blockchain);
 
+  // Minting NFT function
+  const mintNFT = (_account, _name) => {
+    blockchain.gearToken.methods
+      .createRandomGear(_name)
+      .send({from: _account, value: 1000000000000000000})
+      .once("error", (err) => {
+        console.log(err); // Log the error if (`once`) is used
+      })
+        .then((receipt) => {
+        console.log(receipt);
+        dispatch(fetchData());
+      });
+  };
+
   useEffect(() => {
-    if(blockchain.account != "") {
-      dispatch(fetchData());
+    if(blockchain.account !== "") {
+      dispatch(fetchData(blockchain.account));
     }
   },[blockchain.account]); // Run this useEffect whenever the blockchain's account is updated
 
-  return <s.Screen>
-    {blockchain.account !== "" || blockchain.gearToken !== null ? (
-      <s.Container flex={1} ai={"center"} jc={"center"}>
-        <s.TextTitle>
-          Connect to our game
-        </s.TextTitle>
-        <s.SpacerSmall />
-        <button onClick={(e) => {
-          e.preventDefault(); // prevent default form submit
+return <s.Screen>
+  {blockchain.account === "" || blockchain.gearToken === null ? (
+    <s.Container flex={1} ai={"center"} jc={"center"}>
+    <s.TextTitle>Connect to our game</s.TextTitle>
+    <s.SpacerSmall />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
           dispatch(connect());
-        }}>CONNECT</button>
+        }}
+      >
+        CONNECT
+      </button>
+      <s.SpacerSmall />
+      <s.TextTitle>Welcome to our game</s.TextTitle>
+      <s.SpacerSmall />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            mintNFT(blockchain.account, "Gear");
+          }}
+        >
+          CREATE GEAR NFT
+        </button>
+      <s.SpacerMedium />
+    </s.Container>    
+  ) : (
+    <s.Container ai={"center"} style={{ padding: "24px" }}>
+      <s.TextTitle>Welcome to our game</s.TextTitle>
+        <s.SpacerSmall />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              mintNFT(blockchain.account, "Gear");
+            }}
+          >
+            CREATE GEAR NFT
+          </button>
+        <s.SpacerMedium />
+      </s.Container>
+  )}
+  </s.Screen>
+}
+
+/*
+  return <s.Screen>
+    {blockchain.account === "" || blockchain.gearToken === null ? (
+      <s.Container flex={1} ai={"center"} jc={"center"}>
+        <s.TextTitle>Connect to our game</s.TextTitle>
+        <s.SpacerSmall />
+        <button
+          onClick={(e) => {
+            e.preventDefault(); // prevent default form submit
+            dispatch(connect());
+          }}
+        >
+          CONNECT
+        </button>
+        <s.SpacerSmall />
       </s.Container>
     ) : (
       <s.Container ai={"center"} style={{ padding: "24px" }}>
-        <s.TextTitle>
-          Welcome to our game
-        </s.TextTitle>
+        <s.TextTitle>Welcome to our game</s.TextTitle>
         <s.SpacerSmall />
+        <button
+          onClick={(e) => {
+            e.preventDefault(); 
+            mintNFT(blockchain.account, "Gear"); // `"Gear"` is the name of the NFT being minted -> create function to generate random name (variable) later TO-DO TODO To Do
+          }}
+        >
+          CREATE/MINT GEAR NFT
+        </button>
+        <s.SpacerMedium />
       </s.Container>
     )}
   </s.Screen>
 }
-
+*/
 export default App;
