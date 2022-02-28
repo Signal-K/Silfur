81aad3397bb215668a305b80f06c4eddf93cb28e
Gizmotronn <droidology@allianceofdroids.org.au>
2022-02-28 16:37:16 +0800

    #3 Update for styles

--- kinetikakoin-frontend/src/App.js
+++ kinetikakoin-frontend/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "./redux/blockchain/blockchainActions";
+import * as s from "./styles/globalStyles.js";
 
 function App() {
   const dispatch = useDispatch();
@@ -15,21 +16,12 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Account: {blockchain.account}.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Our game
+      </p>
+      <button>Connect
+      </button>
     </div>
   );
 }
--- kinetikakoin-frontend/src/index.js
+++ kinetikakoin-frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import "./styles/reset.css";
+import "./styles/theme.css";
 
 ReactDOM.render(
   <Provider store={store}> {/* The entire app will have access to the redux state*/}
